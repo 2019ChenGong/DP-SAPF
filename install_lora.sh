dceac86648beb80cd5ae9a95b8d8bb8d125bec37
Chen Gong <cheng_abc@outlook.com>
2026-02-06 05:53:14 -0500

    feat: add install_lora.sh

--- install_lora.sh
+++ install_lora.sh
@@ -5,5 +5,5 @@ eval "$(conda shell.bash hook)"
 
 conda create -n dplora python=3.11 -y
 conda activate dplora;
-pip install -r requirements.txt;
+pip install -r requirements_lora.txt;
 cd dm-lora/fast-differential-privacy; pip install -e .;
