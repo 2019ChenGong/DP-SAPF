150d578380106aa31f28d2419c63cc04e361e96c
Chen Gong <cheng_abc@outlook.com>
2026-05-28 10:22:47 -0400

    update:readme

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 set -e
 
 eval "$(conda shell.bash hook)"
-
+mkdir exp
 conda create -n dpimagebench python=3.9 -y
 conda activate dpimagebench;
 pip install torch==2.1.0 torchvision==0.16.0 torchaudio==2.1.0 --index-url https://download.pytorch.org/whl/cu121;
