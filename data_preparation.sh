cb8295411bb80604e79a3785e61ea457de4faf5d
Chen Gong <cheng_abc@outlook.com>
2026-05-28 05:35:09 -0400

    update:readme

--- data_preparation.sh
+++ data_preparation.sh
@@ -1,5 +1,7 @@
+eval "$(conda shell.bash hook)"
+
 conda activate dpimagebench;
 mkdir dataset;
 cd data;
-python preprocess_dataset.py --data_name cifar10_32; cd ..
+python preprocess_dataset.py --data_name cifar10; cd ..
 conda deactivate;
